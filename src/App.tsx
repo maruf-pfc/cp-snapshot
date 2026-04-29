787f7af680217a77faf3fc52bfb01fa3d4e88834
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-04-29 20:03:54 +0600

    chore: scaffold project with Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl font-bold text-gray-800 mt-10">
+        CP Snapshot
+      </h1>
+    </>
+  );
+}
+
+export default App;
